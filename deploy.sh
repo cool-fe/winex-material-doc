cf03e3fd71798ae0a4b092c824a454cbba82170f
huzhiwei <865953771@qq.com>
2021-07-02 16:23:14 +0800

    提交一波

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,19 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 
 
 git init
-git config --global init.defaultBranch master
-git config user.name "frontHu"
-git config user.email "405163706@qq.com"
 git add -A
 git commit -m 'deploy'
 
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/cool-fe/winex-material-doc.git master:gh-pages
 
 cd -
-
-rm -rf docs/.vuepress/dist
